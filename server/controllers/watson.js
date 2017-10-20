b21461fcd57391549d35d8a6f994d7198b79c54c
lokiloko <ianw.tkj2@gmail.com>
2017-10-20 12:27:52 +0700

    error handling image url from google or invalid

--- server/controllers/watson.js
+++ server/controllers/watson.js
@@ -2,19 +2,22 @@ var model = require('../models/watson.js')
 
 class WatsonCtrl {
   static analyzeFood(req, res, next) {
-    if(req.body.image_url){
-      console.log(req.body.image_url)
-      model.analyzeFoodUrl(req.body.image_url).then((data)=>{
+    if (req.body.image_url) {
+      if (req.body.image_url.indexOf('google') > -1) {
+        res.status(500).send('Please provide valid URL.\n Image link from google is not accepted');
+      } else {
+        console.log(req.body.image_url)
+        model.analyzeFoodUrl(req.body.image_url).then((data) => {
+          res.send(data)
+        }).catch((err) => {
+          console.log(err)
+          res.status(500).send('Something wrong with your link')
+        })
+      }
+    } else {
+      model.analyzeFood(req.file.path).then((data) => {
         res.send(data)
-      }).catch((err)=>{
-        console.log(err)
-        res.send(err)
-      })
-    }
-    else{
-      model.analyzeFood(req.file.path).then((data)=>{
-        res.send(data)
-      }).catch((err)=>{
+      }).catch((err) => {
         console.log(err)
         res.send(err)
       })
